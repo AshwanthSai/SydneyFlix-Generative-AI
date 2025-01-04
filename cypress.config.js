dc6d055b2f9b7861d20b924c62e73f8b0a20dd8b
Ashwanth Sai <ashwanthsai.pottasserysivaprasad@students.mq.edu.au>
2025-01-04 13:53:38 +0530

    Cypress Base URL Bug Fix

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:9000',
+    baseUrl: 'http://localhost:3000',
     setupNodeEvents(on, config) {
       //To log to console. Used for debugging
       on('task', {
